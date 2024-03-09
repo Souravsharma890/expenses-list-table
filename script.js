5456bce4af1de473cc62f4eb11b516ec7921e083
Rahul srivastava <87309230+Rahulsrivastava0124@users.noreply.github.com>
2024-03-09 20:50:38 +0530

    Refactored code to use template literals and added comments to improve readability.

--- script.js
+++ script.js
@@ -1,92 +1,98 @@
-let category = document.getElementById("category")
-let amount_input = document.getElementById("amount_input")
-let date_input = document.getElementById("date_input")
-let add_btn = document.getElementById("add_btn")
-let table_body = document.getElementById("table_body")
+let category = document.getElementById("category");
+let amount_input = document.getElementById("amount_input");
+let date_input = document.getElementById("date_input");
+let add_btn = document.getElementById("add_btn");
+let table_body = document.getElementById("table_body");
 let total_amt = document.querySelector(".total_amt");
 // let form = document.getElementById("form")
+
 
 let expenseLists = [];
 let total_val = 0;
 add_btn.addEventListener("click", () => {
-    const category_val = category.value;
-    const amount_val = Number(amount_input.value);
-    const date_val = date_input.value;
-    if (category_val == "") {
-        alert("incomplete data, select category")
-        return;
-    }
-    if (isNaN(amount_val) || amount_val <= 0) {
-        alert("please enter valid amount")
-        return;
-    }
-    if (date_val == "") {
-        alert("incomplete data, please fill date")
-        return;
-    }
+  const category_val = category.value;
+  const amount_val = Number(amount_input.value);
+  const date_val = date_input.value;
+  if (category_val == "") {
+    alert("incomplete data, select category");
+    return;
+  }
+  if (isNaN(amount_val) || amount_val <= 0) {
+    alert("please enter valid amount");
+    return;
+  }
+  if (date_val == "") {
+    alert("incomplete data, please fill date");
+    return;
+  }
 
+  expenseLists.push({ category_val, amount_val, date_val });
+  total_val += amount_val;
 
-    expenseLists.push(category_val, amount_val, date_val);
-    total_val += amount_val;
+  total_amt.textContent = total_val;
+  let table_row = table_body.insertRow();
+  let count = 0;
 
+  const category_cell = table_row.insertCell();
+  category_cell.setAttribute("id", `cat_${count}`);
+  category_cell.textContent = expenseLists[count].category_val;
+  const amount_cell = table_row.insertCell();
+  amount_cell.setAttribute("id", `amt_${count}`);
+  amount_cell.textContent = expenseLists[count].amount_val;
+  const date_cell = table_row.insertCell();
+  date_cell.setAttribute("id", `date_${count}`);
+  date_cell.textContent = expenseLists[count].date_val;
+  const del_cell = table_row.insertCell();
+  del_cell.setAttribute("id", `del_${count}`);
+  count++;
+  console.log(count);
+
+  let del_btn = document.createElement("button");
+
+  del_btn.textContent = "Delete";
+  del_btn.classList.add("del_class");
+  del_btn.addEventListener("click", () => {
+    expenseLists.splice(expenseLists.indexOf(expenseList), 1);
+
+    total_val -= expenseList.amount_val;
     total_amt.textContent = total_val;
-    let table_row = table_body.insertRow();
 
-    const category_cell = table_row.insertCell();
-    const amount_cell = table_row.insertCell();
-    const date_cell = table_row.insertCell();
-    const del_cell = table_row.insertCell();
+    table_body.removeChild(table_row);
+  });
+//   const expenseList = expenseLists(expenseLists.length - 1);
+//   category_cell.textContent = expenseList.category_val;
+//   amount_cell.textContent = expenseList.amount_val;
+//   date_cell.textContent = expenseList.date_val;
+//   del_cell.appendChild.add(del_btn);
+});
 
-    let del_btn = document.createElement("button");
+// for (const expenseList of expenseLists) {
+//   total_val += amount_val;
 
-    del_btn.textContent = "Delete";
-    del_btn.classList.add("del_class");
-    del_btn.addEventListener("click", () => {
-        expenseLists.splice(expenseLists.indexOf(expenseList), 1);
+//   total_amt.textContent = total_val;
+//   let table_row = table_body.insertRow();
 
-        total_val -= expenseList.amount_val;
-        total_amt.textContent = total_val;
+//   const category_cell = table_row.insertCell();
+//   const amount_cell = table_row.insertCell();
+//   const date_cell = table_row.insertCell();
+//   const del_cell = table_row.insertCell();
 
-        table_body.removeChild(table_row);
+//   let del_btn = document.createElement("button");
 
-    })
-    const expenseList = expenseLists(expenseLists.length - 1);
-    category_cell.textContent = expenseList.category_val;
-    amount_cell.textContent = expenseList.amount_val;
-    date_cell.textContent = expenseList.date_val;
-    del_cell.appendChild.add(del_btn);
+//   del_btn.textContent = "Delete";
+//   del_btn.classList.add("del_class");
+//   del_btn.addEventListener("click", () => {
+//     expenseLists.splice(expenseLists.indexOf(expenseList), 1);
 
-})
+//     total_val -= expenseList.amount_val;
+//     total_amt.textContent = total_val;
 
-for (const expenseList of expenseLists) {
-    total_val += amount_val;
-
-    total_amt.textContent = total_val;
-    let table_row = table_body.insertRow();
-
-    const category_cell = table_row.insertCell();
-    const amount_cell = table_row.insertCell();
-    const date_cell = table_row.insertCell();
-    const del_cell = table_row.insertCell();
-
-    let del_btn = document.createElement("button");
-
-    del_btn.textContent = "Delete";
-    del_btn.classList.add("del_class");
-    del_btn.addEventListener("click", () => {
-        expenseLists.splice(expenseLists.indexOf(expenseList), 1);
-
-        total_val -= expenseList.amount_val;
-        total_amt.textContent = total_val;
-
-        table_body.removeChild(table_row);
-
-    })
-    const expenseList = expenseLists(expenseLists.length - 1);
-    category_cell.textContent = expenseList.category_val;
-    amount_cell.textContent = expenseList.amount_val;
-    date_cell.textContent = expenseList.date_val;
-    del_cell.appendChild.add(del_btn);
-
-}
+//     table_body.removeChild(table_row);
+//   });
+//   const expenseList = expenseLists(expenseLists.length - 1);
+//   category_cell.textContent = expenseList.category_val;
+//   amount_cell.textContent = expenseList.amount_val;
+//   date_cell.textContent = expenseList.date_val;
+//   del_cell.appendChild.add(del_btn);
+// }
 console.log(expenseLists);
